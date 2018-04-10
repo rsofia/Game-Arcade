f3238318bb440f67154e72ea185bfa6aa02491a6
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2018-04-10 08:46:47 -0500

    Execute exe

---
+++ Assets/luisquid/Scripts/SCR_ExecuteExe.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+public class SCR_ExecuteExe : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        //Process.Start("C:/Users/Alumno/Desktop/JijiLol.exe");
+	}
+
+    // Update is called once per frame
+    void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            RunGame("C:/Users/Alumno/Desktop/JijiLol.exe");
+        }
+	}
+
+    public void RunGame(string _direction)
+    {
+        Process.Start(_direction);
+    }
+}
