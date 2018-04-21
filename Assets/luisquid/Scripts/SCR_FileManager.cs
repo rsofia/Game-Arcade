536d7aabf810fc5d7957a6f8782f1a1d9f174751
LB <luisbazan417@gmail.com>
2018-04-21 12:41:44 -0500

    Copy all from directory

--- Assets/luisquid/Scripts/SCR_FileManager.cs
+++ Assets/luisquid/Scripts/SCR_FileManager.cs
@@ -17,12 +17,25 @@ public class SCR_FileManager : MonoBehaviour {
     private string pathExe;
     private string pathDataFolder;
     private string pathImg;
+    private string uploadName;
 
     public string [] GetAllFoldersFromPath(string _path)
     {
         string[] DirectoriesInPath = Directory.GetDirectories(_path);
 
         return DirectoriesInPath;
+    }
+
+    public void CopyAllFromDirectory(string _sourcePath, string _destinationPath)
+    {
+        foreach (string dirPath in Directory.GetDirectories(_sourcePath, "*",
+                   SearchOption.AllDirectories))
+            Directory.CreateDirectory(dirPath.Replace(_sourcePath, _destinationPath));
+
+        //Copy all the files & Replaces any files with the same name
+        foreach (string newPath in Directory.GetFiles(_sourcePath, "*.*",
+            SearchOption.AllDirectories))
+            File.Copy(newPath, newPath.Replace(_sourcePath, _destinationPath), true);
     }
 
     public void OpenFile()
@@ -39,35 +52,33 @@ public class SCR_FileManager : MonoBehaviour {
 
         pathExe = GameObject.Find("TXTMSH_Exe").GetComponent<TextMeshProUGUI>().text = StandaloneFileBrowser.OpenFilePanel("Upload Exe", "", extensions, false)[0];
 
+        uploadName = Path.GetFileNameWithoutExtension(pathExe);
+
+        if(Directory.Exists(persistentDataPath + "/Games/" + uploadName))
+        {
+            Debug.Log("YA EXISTE SALIR SALIR");
+        }
         //File.Copy(StandaloneFileBrowser.OpenFilePanel("Upload Exe", "", extensions, false)[0], persistentDataPath + "/Games/Prueba.exe");
     }
 
     public void OnUploadDataFolderClicked()
     {
-        pathDataFolder = StandaloneFileBrowser.OpenFolderPanel("Upload Data Folder", "", false)[0];
-
+        pathDataFolder = GameObject.Find("TXTMSH_DataFolder").GetComponent<TextMeshProUGUI>().text = StandaloneFileBrowser.OpenFolderPanel("Upload Data Folder", "", false)[0];
         Debug.Log(pathDataFolder);
     }
 
     public void OnUploadImageClicked()
     {
         var extensions = new[] { new ExtensionFilter("Image File", "img", "png") };
-        pathImg = StandaloneFileBrowser.OpenFilePanel("Upload Game Thumbnail", "", "", false)[0];
+        pathImg = GameObject.Find("TXTMSH_Thumbnail").GetComponent<TextMeshProUGUI>().text = StandaloneFileBrowser.OpenFilePanel("Upload Game Thumbnail", "", "", false)[0];
         Debug.Log(pathImg);
     }
 
     public void OnUploadFilesClicked()
     {
-        File.Copy(pathExe, persistentDataPath + "/Games/Prueba.exe");
-
-        foreach (string dirPath in Directory.GetDirectories(pathDataFolder, "*",
-                    SearchOption.AllDirectories))
-            Directory.CreateDirectory(dirPath.Replace(pathDataFolder, persistentDataPath + "/Games/"));
-
-        //Copy all the files & Replaces any files with the same name
-        foreach (string newPath in Directory.GetFiles(pathDataFolder, "*.*",
-            SearchOption.AllDirectories))
-            File.Copy(newPath, newPath.Replace(pathDataFolder, persistentDataPath + "/Games/"), true);
+        Directory.CreateDirectory(persistentDataPath + "/Games/" + uploadName);
+        File.Copy(pathExe, persistentDataPath + "/Games/" + uploadName + "/" + uploadName + ".exe");
+        CopyAllFromDirectory(pathDataFolder, persistentDataPath + "/Games/" + uploadName + "/" + uploadName + "_Data/");
     }
 
     void Start () 
